16cf2f06238abda7c59ac693e50560c02a0b33e0
Tracie35 <kariukitracie@gmail.com>
2022-06-08 08:29:08 +0300

    done with assignment

---
+++ index.js
@@ -0,0 +1,17 @@
+const divide = ()=> 2000/100;
+// divide (2000,100);
+// const divides= (num1,num2) => {
+//   console.log(`dividing ${num1}`);
+//   console.log(`dividing ${num2}`);
+//   return num1 + num2;
+// }
+// divide(2000,100);
+
+
+  const square = (x => x ** 2);
+  square (2);
+  let x=2
+
+  const add = (parameter1,parameter2)=> parameter1+parameter2;
+
+
